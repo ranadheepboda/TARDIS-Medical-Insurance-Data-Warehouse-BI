9565c90f8513bc9201378c88828ea31e2349fa24
Ranadheep Boda <ranadheep17@gmail.com>
2026-09-09 13:23:44 +0530

    Add files via upload
    Built a Snowflake Schema DWH for UK health insurer (Torus) — 30 SSIS ETL packages, 30–40K monthly records, SSAS Tabular Model, Power BI reporting.

---
+++ TardisDB_LOOKUP.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 20.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{DBD1EC5D-D6D0-4E91-BEF8-FE8353B29728}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {2C8D355F-251F-4B4C-B510-48058CE705B3}
+	EndGlobalSection
+EndGlobal
